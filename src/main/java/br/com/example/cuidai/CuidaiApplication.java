5067a4fdd912b6794b35daa752e5e767910d0c61
Igor Costa <igorcostagomessouza@gmail.com>
2023-05-11 19:38:45 -0300

    Initiate project

---
+++ src/main/java/br/com/example/cuidai/CuidaiApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.cuidai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuidaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuidaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/example/cuidai/CuidaiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.cuidai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuidaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
