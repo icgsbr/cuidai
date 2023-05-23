78d1ba1c868380bd2308a4e956c11f70ff779457
Igor Costa <igorcostagomessouza@gmail.com>
2023-05-22 23:41:52 -0300

    BE007 Enable API to open on browser after startup

--- src/main/java/br/com/example/cuidai/CuidaiApplication.java
+++ src/main/java/br/com/example/cuidai/CuidaiApplication.java
@@ -3,11 +3,46 @@ package br.com.example.cuidai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class CuidaiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CuidaiApplication.class, args);
+        openBrowser("http://localhost:8080");
+    }
+
+    private static void openBrowser(String url) {
+        String os = System.getProperty("os.name").toLowerCase();
+        Runtime runtime = Runtime.getRuntime();
+
+        try {
+            if (os.contains("win")) {
+                runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } else if (os.contains("mac")) {
+                runtime.exec("open " + url);
+            } else if (os.contains("nix") || os.contains("nux") || os.contains("bsd")) {
+                String[] browsers = {"xdg-open", "google-chrome", "firefox", "chromium-browser"};
+                boolean found = false;
+                for (String browser : browsers) {
+                    try {
+                        runtime.exec(new String[]{browser, url});
+                        found = true;
+                        break;
+                    } catch (IOException e) {
+                        // Ignore and try the next one
+                    }
+                }
+                if (!found) {
+                    throw new RuntimeException("No supported browser found on Linux.");
+                }
+            } else {
+                throw new RuntimeException("Unsupported operating system: " + os);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
